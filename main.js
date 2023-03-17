876057d507e386a7f6a3949b849c6e2f3b8e61ef
tamakiii <tamakiii@users.noreply.github.com>
2023-03-17 16:46:54 +0900

    fix main.js

--- main.js
+++ main.js
@@ -7,17 +7,17 @@ async function loginToAppleMusic(username, password) {
   const page = await context.newPage();
 
   await page.goto('https://music.apple.com/');
-	await page.waitForLoadState('networkidle');
-  await page.click('button[data-testid="signInHeaderButton"]');
-  await page.waitForSelector('iframe[id="aid-auth-widget-iFrame"]');
+  await page.waitForSelector('[data-testid="sign-in-button"]');
+  await page.click('[data-testid="sign-in-button"]');
 
-  const signInFrame = await page.frame({ name: 'aid-auth-widget-iFrame' });
-
-  await signInFrame.fill('input[id="account_name_text_field"]', username);
-  await signInFrame.click('button[id="sign-in"]');
-  await signInFrame.waitForSelector('input[id="password_text_field"]');
-  await signInFrame.fill('input[id="password_text_field"]', password);
-  await signInFrame.click('button[id="sign-in"]');
+  await page.waitForSelector('#auth-container');
+  
+  await page.fill('input[name="account_name"]', username);
+  await page.click('button[type="submit"]');
+  
+  await page.waitForSelector('input[name="password"]');
+  await page.fill('input[name="password"]', password);
+  await page.click('button[type="submit"]');
 
   await page.waitForNavigation();
   return { browser, page };
